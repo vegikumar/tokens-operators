37ce27296ce589fa86b6d0eca227e8fec2850b3d
vegi kumar <94340560+vegikumar@users.noreply.github.com>
2023-06-23 22:04:04 +0530

    Create sizes of datatypes.c

---
+++ sizes of datatypes.c
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    std::cout << "Size of char: " << sizeof(char) << " byte\n";
+    std::cout << "Size of int: " << sizeof(int) << " bytes\n";
+    std::cout << "Size of float: " << sizeof(float) << " bytes\n";
+    std::cout << "Size of double: " << sizeof(double) << " bytes\n";
+    return 0;
+}
